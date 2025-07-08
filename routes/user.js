7ac6805de81bcdd6fb7c494da2a3d2ec9f4dcee2
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2025-07-08 09:57:54 +0530

    signup and signin page done

---
+++ routes/user.js
@@ -0,0 +1,35 @@
+const {Router}=require('express');
+const User=require('../models/user');
+
+const router=Router();
+
+router.get("/signin",(req,res)=>{
+    return res.render("signin");
+});
+
+router.get("/signup",(req,res)=>{
+    return res.render("signup");
+});
+
+router.post("/signup",async (req,res)=>{
+    const {fullName,email,password}=req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+
+    return res.redirect("/");
+});
+
+
+router.post("/signin",async (req,res)=>{
+    const {email,password}=req.body;
+    const user=await User.matchPassword(email,password);
+
+    console.log("User",user);
+
+    return res.redirect('/');
+});
+
+module.exports=router;
