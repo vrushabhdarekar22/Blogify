132219f203a39a2fd046e410121d6817658b9495
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2025-07-10 19:55:13 +0530

    comment logic added

--- routes/blog.js
+++ routes/blog.js
@@ -26,20 +26,22 @@ router.get('/add-new',(req,res)=>{
 
 router.get('/:id',async (req,res)=>{
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments=await Comment.find({blogId:req.params.id}).populate("createdBy")
   return res.render('partials/blog',{
     user:req.user,
     blog,
+    comments,
   })
 });
 
 router.post('/comment/:blogId', async (req,res) => {
-  await Comment.create({
-    content:res.body.content,
+  const comment=await Comment.create({
+    content:req.body.content,
     blogId:req.params.blogId,
     createdBy:req.user._id,
   });
 
-  res.redirect(`blog/${req.params.blogId}`)
+  res.redirect(`/blog/${comment.blogId}#comment`)
 })
 
 router.post('/',upload.single('coverImage'), async(req,res)=>{
